8a7e9505f7feccf177ac52314df65d1877fd7712
mriliujy <mriliujy@gmail.com>
2022-09-29 17:17:49 +0800

    feat(*): first push

---
+++ src/pages/routes.ts
@@ -0,0 +1,19 @@
+import Create from './Create';
+import Setting from './Setting';
+import Login from './Login';
+import Project from './Project';
+
+interface RouteItem {
+  key: string;
+  path: string;
+  component: any;
+}
+
+const routes: RouteItem[] = [
+  { key: 'project', path: '/project', component: Project },
+  { key: 'create', path: '/create', component: Create },
+  { key: 'setting', path: '/setting/:id', component: Setting },
+  { key: 'login', path: '/login', component: Login },
+]
+
+export default routes;
